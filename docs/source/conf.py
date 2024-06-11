89254c96ef8ec0048176da9653350a08b9e81fcf
Joshua <jpic@umich.edu>
2024-06-11 14:48:19 -0400

    changing build sys path to include BRAD hopefully

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
-# sys.path.insert(0, os.path.abspath('../../BRAD/'))
+sys.path.insert(0, os.path.abspath('../../BRAD/'))
 print('Building Documentation. Checking path:')
 print(sys.path)
 
