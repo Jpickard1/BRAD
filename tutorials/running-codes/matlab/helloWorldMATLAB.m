a22b6065a4a17d9d74c175ecb3edd536960ed806
Joshua <jpic@umich.edu>
2024-06-14 16:11:17 -0400

    matlab functions and BioPipelineDesigner codes are working

---
+++ tutorials/running-codes/matlab/helloWorldMATLAB.m
@@ -0,0 +1,9 @@
+function output = helloWorldMATLAB()
+% Print "Hello World"
+%
+% Author: Joshua Pickard
+%         jpic@umich.edu
+% Date: June 14, 2024
+    disp('Hello World! This message is generated from BRAD running the helloWorldMATLAB function!');
+    output = 0;
+end
