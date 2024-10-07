bd1efb2ebef6852f7bf06ac3954f56d8b8c6e11c
Joshua <jpic@umich.edu>
2024-10-06 23:20:11 -0400

    removing commented out imports

--- BRAD/__init__.py
+++ BRAD/__init__.py
@@ -9,13 +9,3 @@ __all__     = ['brad',
                'scraper',
               ]
 
-# from .brad import *
-
-# import nltk
-# from nltk.stem import WordNetLemmatizer
-# from nltk.corpus import wordnet
-
-# We only need to do this when installing the package
-# nltk.download('words')
-# nltk.download('wordnet')
-# nltk.download('omw-1.4')
