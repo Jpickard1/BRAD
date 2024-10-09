95d97a1ede6f46fd9b07c851f6df82d6331768d7
Joshua <jpic@umich.edu>
2024-10-09 10:32:23 -0400

    refactor(tutorials): tutorials that were not really tutorials (i.e. they were directories using the library) were removed. The remaining tutorials will need to be cleaned up based on recent changes to the code. However, we should restrucutre how/what these look like because: (1) we now have a decent GUI, (2) significant features of the code and how a user should interacte with it have changes, and (3) the rest of the documentaiton system was changed

--- tutorials/control/pendcart.m
+++
@@ -1,8 +0,0 @@
-function dx = pendcart(x,m,M,L,g,d,u)
-Sx = sin(x(3));
-Cx = cos(x(3));
-D = m*L*L*(M+m*(1-Cxˆ2));
-dx(1,1) = x(2);
-dx(2,1) = (1/D)*(-mˆ2*Lˆ2*g*Cx*Sx + m*Lˆ2*(m*L*x(4)^2*Sx - d*x(2))) + m*L*L*(1/D)*u;
-dx(3,1) = x(4);
-dx(4,1) = (1/D)*((m+M)*m*g*L*Sx - m*L*Cx*(m*L*x(4)^2*Sx - d*x(2))) - m*L*Cx*(1/D)*u;
